2ebe2f1875879e312f685bcc3861b8fe55498ed2
SeongokRyu <seongokryu@galux.co.kr>
2025-01-11 09:21:10 +0900

    250111 update

--- miscellaneous/submit.sh
+++ miscellaneous/submit.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#SBATCH -J inf_gin_0.25_max_10.0
+#SBATCH -J inference_
 #SBATCH -p normal.q
 #SBATCH --nice=1000
-#SBATCH --nodelist star029
+#SBATCH --nodelist star028
 #SBATCH -N 1
 #SBATCH -n 128
 #SBATCH -o %x.out
@@ -10,4 +10,4 @@
 
 
 # Hidden dimension
-python -u ongkoo_score.py -m us -d 2025-01-07  > ongkoo_us.log
+python -u ongkoo_score.py -m us -d 2025-01-11  > ongkoo_us.log
